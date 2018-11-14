96d9854f21ff9f1de54b4f2a79a24dc3fb28817c
Dror Gensler <dror@slipstream.cloud>
2018-11-14 22:58:42 +0000

    tank-like controls translation, pre-test

--- mouse-control.py
+++ mouse-control.py
@@ -16,22 +16,44 @@ req.connect(True, "random")
 #uart - octal 3 digit numbers, 012 is nl delimiter
 #req.write_by_handle(0x002a, str(bytearray([101,012,102,012,101,012]))) # efe - array
 
-def callback():
-    print("callback")
-    return
-
 while True:
 #Mouse
   data = display.Display().screen().root.query_pointer()._data
-  print("X:", data["root_x"])
-  print("Y:", data["root_y"])
+
+  try:
+      olddata
+  except:
+      olddata = data
+
+  #print("X:", data["root_x"])
+  #print("Y:", data["root_y"])
+  #print("X:", data["root_x"])
+  #print("Y:", data["root_y"])
+  
+  #forward/back
+  right = data["root_y"] - olddata["root_y"]
+  left = olddata["root_y"] - data["root_y"]
+
+  #turn
+  right = right + (data["root_x"] - olddata["root_x"])
+  left = left + (data["root_x"] - olddata["root_x"])
+
+  print("left: ", left)
+  print("right: ", right)
+
+  time.sleep(0.5)
+
+  #req.write_by_handle_async(0x002a, str(bytearray([key,012])), response)
+  #while not response.received():
+  #    time.sleep(0.01)
+
 
   #key = ord(readchar.readchar())
-  key = ord("q")
+  #key = ord("q")
 
-  req.write_by_handle_async(0x002a, str(bytearray([key,012])), response)
-  while not response.received():
-      time.sleep(0.1)
+  #req.write_by_handle_async(0x002a, str(bytearray([key,012])), response)
+  #while not response.received():
+  #    time.sleep(0.01)
 
 
   #try:
