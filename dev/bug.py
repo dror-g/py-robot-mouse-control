1935480b3367074b077c87ed81d213ea02bad758
Dror Gensler <dror@slipstream.cloud>
2018-11-15 13:48:11 +0000

    cleanup

--- dev/bug.py
+++
@@ -1,5 +0,0 @@
-import bluetooth
-x = bluetooth.discover_devices()
-n = bluetooth.lookup_name(x[0])
-print(n)
-
--- dev/galib-scan.py
+++
@@ -1,7 +0,0 @@
-from gattlib import DiscoveryService
-
-service = DiscoveryService("hci0")
-devices = service.discover(2)
-
-for address, name in devices.items():
-    print("name: {}, address: {}".format(name, address))
